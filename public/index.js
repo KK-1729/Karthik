58061a1007ed0b3fc6864bfdfaa681a8d8f4a8c3
Karthik <kkarthikmadduri1729@gmail.com>
2021-04-23 22:54:30 +0530

    first commit

---
+++ public/index.js
@@ -0,0 +1,69 @@
+var animate = ScrollReveal();
+
+animate.reveal('.right, .nav-right , #skills, #projects' , {
+    delay: 500
+}); 
+animate.reveal('.navbar-brand, .nav-link', {
+    delay: 750
+});
+animate.reveal('.left-name', {
+    delay: 1250,
+    duration: 3000, 
+    origin:'left', 
+    distance: '100px'
+});
+animate.reveal('.caption-left', {
+    delay: 2500
+});
+animate.reveal('.right-name', {
+    delay: 1500, 
+    duration: 3000, 
+    origin: 'right', 
+    distance:'100px'
+});
+animate.reveal('.caption-right', {
+    delay: 3500
+});
+animate.reveal('.about-head, .skills-head, .projects-head, #footer a', {
+    delay: 1000
+});
+animate.reveal('.picture', {
+    delay: 1500, 
+    duration: 2500, 
+    origin: 'left', 
+    distance: '100px'
+});
+animate.reveal('.about-content-text', {
+    delay: 2000, 
+    duration: 2500, 
+    origin: 'right', 
+    distance: '100px'
+});
+animate.reveal('.left-circle, .left-triangle, .right-triangle', {
+    delay: 1500
+});
+animate.reveal('.right-circle', {
+    delay: 1500
+});
+animate.reveal('.scroll-up', {
+    delay: 1000
+});
+animate.reveal('.left-card', {
+    delay: 2000, 
+    duration: 3000, 
+    origin:'left', 
+    distance: '200px'
+});
+animate.reveal('.right-card', {
+    delay: 2000, 
+    duration: 3000, 
+    origin:'right', 
+    distance: '200px'
+});
+animate.reveal('.middle-card', {
+    delay: 2000, 
+    duration: 3000, 
+    origin:'bottom', 
+    distance: '200px'
+});
+
